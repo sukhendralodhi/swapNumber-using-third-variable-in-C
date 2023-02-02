8e751e51209fc5f46648be281df7a1d62ef86495
Sukhendra S Lodhi <95135906+sukhendralodhi@users.noreply.github.com>
2023-02-02 04:27:49 +0000

    without using third variable

--- main.c
+++ main.c
@@ -5,13 +5,13 @@ int main(void) {
   printf("Enter the number1 and number2: ");
   scanf("%d%d",&number1, &number2);
 
-  // number1 = number1 + number2;
-  // number2 = number1 - number2;
-  // number1 = number1 - number2;
+  number1 = number1 + number2;
+  number2 = number1 - number2;
+  number1 = number1 - number2;
 
-  int temp = number1;
-  number1 = number2;
-  number2 = temp;
+  // int temp = number1;
+  // number1 = number2;
+  // number2 = temp;
   
   printf("\nAfter swaping: number1 = %d number2 = %d", number1, number2);
   return 0;
